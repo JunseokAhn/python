1be151b5117d3f61eeb4e4b56be767ab120a2188
JunseokAhn <57289429+JunseokAhn@users.noreply.github.com>
2020-06-25 14:33:19 +0900

    Create hello world

---
+++ hello world.py
@@ -0,0 +1,26 @@
+print("Hello World!")
+
+if True:
+    print("OK")
+
+#주석
+"""여러줄
+주석하는
+글자
+"""
+
+a=12
+b=34
+print(a, b, sep='')
+
+print(a, end='')
+print(b)
+
+age = input("몇살이세요")
+print(age)
+
+print(2**100000)
+
+print(hex(26))
+
+print(0b1101)
