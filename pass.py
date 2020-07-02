c418223925c8d3879be61484ecf077e9567e03de
JunseokAhn <57289429+JunseokAhn@users.noreply.github.com>
2020-07-02 12:51:15 +0900

    Add Pass

---
+++ pass.py
@@ -0,0 +1,20 @@
+print("첫번째 문장")
+pass
+print("두번째 문장")
+
+def test():
+    pass
+
+test()
+
+# def test2():
+    # 이렇게만 쓰면 에러
+
+# test2()
+
+score = 90
+
+if score>90:
+    pass
+else:
+    print(90)
